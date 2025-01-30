d012ca9a6578edd2f09f182847179858e77b9de9
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-01-30 17:44:23 -0300

    a prueba de bruno

--- front/src/pages/FormRegister.tsx
+++ front/src/pages/FormRegister.tsx
@@ -2,42 +2,50 @@ import Link from "next/link";
 
 const FormRegister = () => {
     return(
-        <div className="flex justify-center items-center">
-            <div className="flex flex-col bg-red-600">
+        <div className="flex justify-center items-center h-min-screen">
+            <div className="flex flex-col bg-red-600 h-auto px-3 py-2">
                 <div>
                     <ul>
-                        <li>
-                            <label htmlFor="">Nombre: </label>
-                            <input className="" 
-                                type="text"
-                                id="name"
-                                name="name"
-                            />
-                        </li>
-                        <li>
-                            <label htmlFor="">Apellido: </label>
-                            <input className=""
-                                type="text"
-                                id="apellido"
-                                name="apellido"
-                            />
-                        </li>
-                        <li>
-                            <label htmlFor="">Edad: </label>
-                            <input className=""
-                                type="text"
-                                id="edad"
-                                name="edad"
-                            />
-                        </li>
-                        <li>
-                            <label htmlFor="">Dirección: </label>
-                            <input className="" 
-                                type="text"
-                                id="dirreccion"
-                                name="direccion"
-                            />
-                        </li>
+                        <div className="p-1">
+                            <li>
+                                <label htmlFor="">Nombre: </label>
+                                <input className="rounded-sm" 
+                                    type="text"
+                                    id="name"
+                                    name="name"
+                                />
+                            </li>
+                        </div>
+                        <div className="p-1">
+                            <li>
+                                <label htmlFor="">Apellido: </label>
+                                    <input className="rounded-sm"
+                                        type="text"
+                                    id="apellido"
+                                    name="apellido"
+                                />
+                            </li>
+                        </div>
+                        <div className="p-1">
+                            <li>
+                                <label htmlFor="">Edad: </label>
+                                <input className="rounded-sm"
+                                    type="text"
+                                    id="edad"
+                                    name="edad"
+                                />
+                            </li>
+                        </div>
+                        <div className="p-1">
+                            <li>
+                                <label htmlFor="">Dirección: </label>
+                                <input className="rounded-sm" 
+                                    type="text"
+                                    id="dirreccion"
+                                    name="direccion"
+                                />
+                            </li>
+                        </div>
                     </ul>
                 </div>
                 <div className="flex justify-center items-center">
