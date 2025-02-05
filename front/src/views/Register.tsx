71820d8768e611b132f975eea4129b46b1782f9b
NicolasDelFabro <nicolasdelfabro123@gmail.com>
2025-02-05 16:54:32 -0300

    form register

--- front/src/views/Register.tsx
+++ front/src/views/Register.tsx
@@ -3,7 +3,30 @@ import Link from "next/link";
 const Register = () => {
     return(
         <div>
-            <h1>REGISTER</h1>
+            <div>
+                <ul>
+                    <li>
+                        <label htmlFor="name">Nombre: </label>
+                        <input type="text" name="mane" id="name"/>
+                    </li>
+                    <li>
+                        <label htmlFor="apellido">Apellido: </label>
+                        <input type="text" name="apellido" id="apellido"/>
+                    </li>
+                    <li>
+                        <label htmlFor="domicilio">Domicilio: </label>
+                        <input type="text" name="domicilio" id="domicilio" />
+                    </li>
+                    <li>
+                        <label htmlFor="email">Correo: </label>
+                        <input type="email" name="email" />
+                    </li>
+                    <li>
+                        <label htmlFor="password">Contraseña: </label>
+                        <input type="password" name="password" id="password" />
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
